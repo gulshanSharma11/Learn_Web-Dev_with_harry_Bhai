291ccd11e0022e794010f379c4c1749ceb639c72
GULSHAN SHARMA <72249304+gulshanSharma11@users.noreply.github.com>
2024-02-25 23:57:38 +0530

    ### Commit message
    - Added HTML, CSS, and JavaScript files to create a simple password generator app.
    - The app generates a random password of length 28 characters, including uppercase letters, lowercase letters, numbers, and symbols.
    - The generated password is displayed in an input field and can be copied to the clipboard by clicking the copy icon. (generated by blackbox.ai )

---
+++ javascript-projects/Notes-App/script.js
@@ -0,0 +1,8 @@
+const toggle=document.getElementById("content");
+function deletee(){
+    toggle.style.display="none";
+}
+function add(){
+   var x = document.createElement(input);
+   document.querySelector("#content").appendChild(x)  ;
+}
---
+++ javascript-projects/Password Generator-App/script.js
@@ -0,0 +1,25 @@
+const password=document.getElementById("password");
+
+
+const upperCase="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase="abcdefghijklmnopqrstuvwxyz";
+const numbers="0123456789";
+const Symbol="!@#$%^&*()_+=-~<>?/";
+let final='';
+const length=28;
+let newPassword=upperCase+lowerCase+numbers+Symbol;
+function generateRandomString(){
+    let randomNumber='';
+    while(randomNumber.length<length){
+    randomNumber += newPassword[Math.floor(Math.random()*newPassword.length)];
+    }
+    password.value=randomNumber;
+}
+function copy(){
+    password.select();
+    document.execCommand('copy');
+}
+
+
+
+// Generate and log a random string of 8 characters
