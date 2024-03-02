cd47e4a1df16eadbc0d3b9bdc139ef39f555fbfb
GULSHAN SHARMA <72249304+gulshanSharma11@users.noreply.github.com>
2024-03-02 23:10:49 +0530

    Refactor Node.js file by removing unused code and adding a simple HTTP server. (generated by blackbox.ai )

--- Node/file.js
+++ Node/file.js
@@ -1,23 +1,34 @@
-const a=4;
-const b=5;
-var c = a+b;
-console.log(c);
-function main(a,b){
-    return a+b;
-}
-main(4,5);
+// const a=4;
+// const b=5;
+// var c = a+b;
+// console.log(c);
+// function main(a,b){
+//     return a+b;
+// }
+// main(4,5);
 
-const fs=require("fs");
+// const fs=require("fs");
 
-//fs.writeFileSync("./index.txt","console.log(hello Sir)");
- // how to export this file?
- fs.readFileSync("./index.txt",(err)=>{});
+// //fs.writeFileSync("./index.txt","console.log(hello Sir)");
+//  // how to export this file?
+//  fs.readFileSync("./index.txt",(err)=>{});
  
 
-const data=fs.readFileSync('index.txt',"utf-8" );
-console.log(data);
-fs.appendFileSync("./index.txt","/nJai Ram Ji  kI");
-fs.unlinkSync("./index.txt");
+// const data=fs.readFileSync('index.txt',"utf-8" );
+// console.log(data);
+// fs.appendFileSync("./index.txt","/nJai Ram Ji  kI");
+// fs.unlinkSync("./index.txt");
+
+// const os=require("os");
+// console.log(os.cpus().length);
 
 
+const http=require("http");
+
+const myServer=http.createServer((req, res)=>{
+    console.log("req successful");
+    res.end("Hello i am Server");
+});
+
+myServer.listen(5600);
 
