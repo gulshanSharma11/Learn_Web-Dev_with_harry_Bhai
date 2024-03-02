94d87a9635f9933e52cb5b975dc2b4ca821ea9c7
GULSHAN SHARMA <72249304+gulshanSharma11@users.noreply.github.com>
2024-03-02 22:28:43 +0530

    Add

---
+++ Node/hello.js
@@ -0,0 +1,10 @@
+console.log("Hello World");
+const x=10;
+const b=4;
+var c=x+b;
+console.log(c);
+
+c=c+b;
+console.log(c);
+
+   
--- Node/intro.js
+++
@@ -1,4 +0,0 @@
-console.log("Hello js");
-let a = 4;
-let b = 3+a;
-console.log(b);
