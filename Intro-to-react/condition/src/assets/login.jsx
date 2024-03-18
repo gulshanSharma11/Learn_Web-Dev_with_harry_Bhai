92203544901545991d41a41775079cdb6f5d938a
GULSHAN SHARMA <72249304+gulshanSharma11@users.noreply.github.com>
2024-03-18 22:49:46 +0530

    Added a counter button that increments the count state and displays the current count. Added a button that toggles the display of another button. Added a comment to explain the purpose of the code. Updated the useEffect hook to use the useRef hook to keep track of the current count.

---
+++ Intro-to-react/condition/src/assets/login.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Show =  ()=>{
+  return (<>
+  <h1>login</h1>
+  <button>Clixk me show the result</button>
+  </>)
+}
+
+export default Show
--- Intro-to-react/vite-props-componenets/react-com/prop/src/App.jsx
+++ Intro-to-react/vite-props-componenets/react-com/prop/src/App.jsx
@@ -1,12 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState,useEffect } from 'react'
+
 import BasicExample from './components/Card.jsx'
 import NavScrollExample from './components/Navbar.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useRef } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  // useEffect(()=>{
+
+  // //  let a= document.getElementById('new');
+  // //  a.innerHTML=Math.random();
+  // // function main(){
+  // //   <BasicExample title="Java" description="Hello Some quick example text to build on the card title and make up the bulk of the card's content."/>
+  // // }
+  //  let a =document.getElementById('new')
+  //  a.appendChild(document.createElement("p"));
+  // },[count]);
+  const a=useRef();
+  // useEffect(()=>{
+  //   alert(`count has changed ${count}`)
+  //   a.current=a.current+1;
+  //   console.log(a.current);
+  //   alert(a.current);
+  // },[count])
+  
+  const [togle,setTogle]=useState(false);
+  
+
 
   return (
     <>
@@ -17,28 +38,32 @@ function App() {
    <BasicExample title="JavaScript" description="JavaScript is the world's most popular programming language.JavaScript is the programming language of the Web."/>
    <BasicExample title="React" description="Hello Some quick example text to build on the card title and make up the bulk of the card's content."/>
    </div>
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+
+   <button style={{margin:" 40px 40%"}} ref={a}  onClick={()=> setCount(count+1)}> Count value is {count}</button>
+   <button style={{margin:" 40px 40%"}} onClick={()=> {a.current.style.display="none"}}>click me</button>
+   {/* <div style={{height:"40px",width:"20px",backgroundColor:"yello" }} id='new'>Hello  i ma 20 </div> */}
+
+
+
+   {togle?<button>togle true</button>:<button>togle false</button>}
+   <button onClick={()=> setTogle(!togle)}>togle switcher</button>
+
+
+
+
+
+   
+   
+   
+   
+
+   
+
+
+      
     </>
   )
+  
 }
 
 export default App
