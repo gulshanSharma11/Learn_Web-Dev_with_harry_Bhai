b07419a684d96cee24f7a4d84a0158fa3f1e1e79
GULSHAN SHARMA <72249304+gulshanSharma11@users.noreply.github.com>
2024-03-22 23:46:21 +0530

    "Add homepage components for finding doctor, location, and booking appointment; add About section and Services section with ServiceList component to Home.jsx; export Services component in Services.jsx"

--- Full-Stack-Projects/frontend/src/pages/Home.jsx
+++ Full-Stack-Projects/frontend/src/pages/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
-import '../App.css';
+
 import heroImg01 from "../assets/images/hero-img01.png";
 import heroImg02 from "../assets/images/hero-img02.png";
 import heroImg03 from "../assets/images/hero-img03.png";
 import icon01 from "../assets/images/icon01.png";
+import icon02 from "../assets/images/icon02.png";
+import icon03 from "../assets/images/icon03.png";
+import {BsArrowRight} from 'react-icons/bs'
+import {Link} from 'react-router-dom';
+import About from '../components/About/about';
+import ServiceList from '../components/Services/ServiceList';
+
 
 
 
@@ -92,10 +99,74 @@ const Home = () => {
         <div className="flex items-center justify-center">
           <img src={icon01} alt="" />
         </div>
+        <div className="mt-[30px]">
+          <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">
+            Find a Doctor
+          </h2>
+          <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">
+            World-class care for everyone. Our health System offers
+            Unmatched, expert health care. From the lab to the clinic.
+          </p>
+          <Link to='/doctors' className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none">
+            <BsArrowRight className='group-hover:text-white w-6 h-5'/>
+          </Link>
+        </div>
+      </div>
+      <div className="py-[30px] px-5">
+        <div className="flex items-center justify-center">
+          <img src={icon02} alt="" />
+        </div>
+        <div className="mt-[30px]">
+          <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">
+            Find a Location
+          </h2>
+          <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">
+            World-class care for everyone. Our health System offers
+            Unmatched, expert health care. From the lab to the clinic.
+          </p>
+          <Link to='/doctors' className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none">
+            <BsArrowRight className='group-hover:text-white w-6 h-5'/>
+          </Link>
+        </div>
+      </div>
+      <div className="py-[30px] px-5">
+        <div className="flex items-center justify-center">
+          <img src={icon03} alt="" />
+        </div>
+        <div className="mt-[30px]">
+          <h2 className="text-[26px] leading-9 text-headingColor font-[700] text-center">
+            Book Appointment
+          </h2>
+          <p className="text-[16px] leading-7 text-textColor font-[400] mt-4 text-center">
+            World-class care for everyone. Our health System offers
+            Unmatched, expert health care. From the lab to the clinic.
+          </p>
+          <Link to='/doctors' className="w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none">
+            <BsArrowRight className='group-hover:text-white w-6 h-5'/>
+          </Link>
+        </div>
       </div>
     </div>
   </div>
 </section>
+
+<About/>
+
+{/*============ Service section ============= */}
+<section>
+  <div className="container">
+    <div className="xl:w-[470px] mx-auto">
+      <h2 className="heading text-center">Our medical services</h2>
+      <p className="text-para text-center">
+        World-class care for everyone. Our health System offers unmatched,
+        expert health care.
+      </p>
+    </div>
+    <ServiceList/>
+  </div>
+</section>
+
+{/*============ Service section end ============= */}
 
       </>
    
