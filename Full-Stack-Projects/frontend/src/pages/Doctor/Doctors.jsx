c4833b97140745f18da1b51a714e2ab129db7af2
GULSHAN SHARMA <72249304+gulshanSharma11@users.noreply.github.com>
2024-03-27 01:53:18 +0530

    "Add doctor details page with doctor about and feedback tabs"
    This commit adds a new page for displaying doctor details, including a doctor's image, title, rating, and a brief description. The page also includes two tabs for displaying information about the doctor: an "About" tab and a "Feedback" tab. The tab content is managed using the useState hook in React. The DoctorAbout and Feedback components are imported and rendered based on the selected tab. The layout of the doctors grid has also been changed to have three columns instead of four for larger screen sizes.

--- Full-Stack-Projects/frontend/src/pages/Doctor/Doctors.jsx
+++ Full-Stack-Projects/frontend/src/pages/Doctor/Doctors.jsx
@@ -17,7 +17,7 @@ const Doctors = () => {
       </section>
       <section>
         <div className="container">
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 ">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  ">
           {doctors.map(doctor=>(
         <DoctorCard key={doctor.id} doctor={doctor}/>
     ))}
