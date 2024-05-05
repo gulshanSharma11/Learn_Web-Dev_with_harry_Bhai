345155229dd52d798838c8c005dc1894a868a083
GULSHAN SHARMA <72249304+gulshanSharma11@users.noreply.github.com>
2024-05-05 20:21:40 +0530

    addd

--- Full-Stack-Projects/frontend/src/pages/Signup.jsx
+++ Full-Stack-Projects/frontend/src/pages/Signup.jsx
@@ -1,52 +1,194 @@
-import React, { useState } from 'react';
-import { toast } from 'react-toastify';
+import React from "react";
+import signupImg from "../assets/images/signup.gif";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import uploadImageToCloudinary from "../utils/uploadCloudinary";
+import { BASE_URL } from "../config.js";
+import { toast } from "react-toastify";
 import HashLoader from 'react-spinners/HashLoader';
-import uploadImageToCloudinary from '../utils/uploadCloudinary';
 
-const ImageUploader = ({ onImageUpload }) => {
+const Signup = () => {
+  const [selectFile, setSelectedFile] = useState(null);
+  const [previewURL, setPreviewURL] = useState("");
   const [loading, setLoading] = useState(false);
-  const [previewURL, setPreviewURL] = useState('');
+
   const [formData, setFormData] = useState({
-    photo: null,
+    name: "",
+    email: "",
+    password: "",
+    photo: selectFile,
+    gender: "",
+    role: "patient",
   });
 
+  const navigate = useNavigate();
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
   const handleFileInputChange = async (event) => {
     const file = event.target.files[0];
+
+    const data = await uploadImageToCloudinary(file);
+
+    setPreviewURL(data.url);
+    setSelectedFile(data.url);
+    setFormData({ ...formData, photo: data.url });
+  };
+  const submitHandler = async event => {
+    event.preventDefault();
     setLoading(true);
     try {
-      const data = await uploadImageToCloudinary(file);
-      setPreviewURL(data.url);
-      setFormData({ photo: data.url });
-      onImageUpload(data.url); // Pass the uploaded image URL to the parent component
+      const res = await fetch(`${BASE_URL}/auth/register`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const { message } = await res.json();
+      if (!res.ok) {
+        throw new Error(message);
+      }
       setLoading(false);
+<<<<<<< HEAD
     } catch (error) {
-      toast.error('Failed to upload image');
+      toast.error('Failed to upload imagess');
+=======
+      toast.success(message);
+      navigate("/login");
+    } catch (err) {
+      toast.error(err.message);
+>>>>>>> b45f03a817621af46f5d457a56bc9a0e96f40247
       setLoading(false);
     }
   };
 
   return (
-    <div className="mb-5 flex items-center gap-3">
-      {previewURL && (
-        <figure className="w-[60px] h-[60px] rounded-full border-2 border-solid border-primaryColor flex items-center justify-center">
-          <img src={previewURL} alt="Uploaded" className="w-full rounded-full" />
-        </figure>
-      )}
-      <div className="relative w-[130px] h-[50px]">
-        <input
-          type="file"
-          onChange={handleFileInputChange}
-          accept=".jpg, .jpeg, .png"
-          className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer"
-        />
-        <label
-          className="absolute top-0 left-0 w-full h-full flex items-center px-[0.75rem] py-0.375rem] text-[15px] leading-6 overflow-hidden bg-[#0066ff46] text-headingColor font-semibold rounded-lg truncate cursor-pointer"
-        >
-          {loading ? <HashLoader size={20} color="#ffffff" /> : 'Upload Photo'}
-        </label>
+    <section className="px-5 xl:px-0">
+      <div className="max-w-[1170px] mx-auto">
+        <div className="grid grid-cols-1 lg:grid-cols-2">
+          <div className="hidden lg:block bg-primaryColor rounded-l-lg">
+            <figure className="rounded-l-lg">
+              <img src={signupImg} alt="" className="w-full rounded-l-lg" />
+            </figure>
+          </div>
+
+          <div className="rounded-l-lg lg:pl-16 py-10">
+            <h3 className="text-headingColor text-[22px] leading-9 font-bold mb-10">
+              Create an <span className="text-primaryColor">account</span>
+            </h3>
+            <form onSubmit={submitHandler}>
+              <div className="mb-5">
+                <input
+                  type="text"
+                  value={formData.name}
+                  onChange={handleInputChange}
+                  placeholder="Full Name"
+                  name="name"
+                  className="w-full px-4 pr-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer"
+                  required
+                />
+              </div>
+              <div className="mb-5">
+                <input
+                  type="email"
+                  value={formData.email}
+                  onChange={handleInputChange}
+                  placeholder="Enter Your Email"
+                  name="email"
+                  className="w-full pr-4 py-3 border-b border-solid px-4 border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer"
+                  required
+                />
+              </div>
+              <div className="mb-5">
+                <input
+                  type="password"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                  placeholder="Password"
+                  name="password"
+                  className=" px-4 w-full pr-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer"
+                  required
+                />
+              </div>
+              <div className="mb-5 flex items-center justify-between">
+                <label className="text-headingColor font-bold text-[16px] leading-7">
+                  Are you a:
+                  <select
+                    name="role"
+                    value={formData.role}
+                    onChange={handleInputChange}
+                    className="text-textColor font-semibold text-[15px] leading-7 px-4 py-3 focus:outline-none"
+                  >
+                    <option value="patient">Patient</option>
+                    <option value="doctor">Doctor</option>
+                  </select>
+                </label>
+
+                <label className="text-headingColor font-bold text-[16px] leading-7">
+                  Gender:
+                  <select
+                    name="gender"
+                    value={formData.gender}
+                    onChange={handleInputChange}
+                    className="text-textColor font-semibold text-[15px] leading-7 px-4 py-3 focus:outline-none"
+                  >
+                    <option value="">Select</option>
+                    <option value="male">Male</option>
+                    <option value="female">Female</option>
+                    <option value="others">Others</option>
+                  </select>
+                </label>
+              </div>
+
+              <div className="mb-5 flex items-center gap-3">
+                { selectFile && <figure className="w-[60px] h-[60px] rounded-full border-2 border-solid border-primaryColor flex items-center justify-center">
+                  <img src={previewURL} className="w-full rounded-full" />
+                </figure>}
+
+                <div className="relative w-[130px] h-[50px]">
+                  <input
+                    type="file"
+                    onChange={handleFileInputChange}
+                    name="photo"
+                    id="customerFile"
+                    accept=".jpg, .jpeg, .png"
+                    className="absolute top-0 left-0 w-full h-full opacity-0 cursor-pointer"
+                  />
+                  <label
+                    htmlFor="customerFile"
+                    className="absolute top-0 left-0 w-full h-full flex items-center px-[0.75rem] py-0.375rem] text-[15px] leading-6 overflow-hidden bg-[#0066ff46] text-headingColor font-semibold rounded-lg truncate cursor-pointer"
+                  >
+                    Upload Photo
+                  </label>
+                </div>
+              </div>
+
+              <div className="mt-7">
+                <button
+                disabled={loading && true}
+                  type="submit"
+                  className="w-full bg-primaryColor text-white text-[18px] leading-[30px] rounded-lg px-4 py-3"
+                >
+                  { loading ? <HashLoader size={35} color="#ffffff"/> : 'Sign Up'}
+                </button>
+              </div>
+              <p className="mt-5 textColor text-center">
+                Already have a account?
+                <Link
+                  to="/login"
+                  className="text-primaryColor font-medium ml-1"
+                >
+                  Login
+                </Link>
+              </p>
+            </form>
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
-export default ImageUploader;
+export default Signup;
